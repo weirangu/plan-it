1b85a661e8122d398fdbedb8689c61c623e80654
Weiran Gu <weirangu@gmail.com>
2019-05-10 20:41:48 -0400

    Added the Course model

--- backend/planit/main/models.py
+++ backend/planit/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    CAMPUS = (
+        ('UTSG', 'St. George'),
+        ('UTM', 'Mississauga'),
+        ('UTSC', 'Scarborough')
+    )
+
+    # The ID is really the course code + term
+    id = models.CharField(max_length=14, primary_key=True)
+
+    code = models.CharField(max_length=9)
+    name = models.TextField()
+    description = models.TextField()
+    faculty = models.TextField()
+    department = models.TextField()
+    prerequisites = models.TextField()
+    exclusions = models.TextField()
+    level = models.PositiveSmallIntegerField()
+    campus = models.CharField(max_length=4, choices=CAMPUS)
+    term = models.CharField(max_length=6)
+    breadth1 = models.PositiveSmallIntegerField()
+
+    # Some courses have 2 breadth requirements
+    breadth2 = models.PositiveSmallIntegerField(null=True)
